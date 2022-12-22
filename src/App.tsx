36a1ffb2ef4e8eb3fd8022aa56719d7369faf950
Pramodh Viswanath <pramodh@pink.gg>
2022-12-22 16:52:19 +0530

    Added project section & few code improvements

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import CardSection from "./components/CardSection";
 import InfoSection from "./components/InfoSection";
 import Navbar from "./components/navbar";
+import ProjectSection from "./components/ProjectSection";
 import { useToggleDarkMode } from "./hooks/hooks";
 
 function App() {
@@ -12,7 +13,50 @@ function App() {
       <div className="min-h-screen px-10 md:px-20 bg-white dark:bg-gray-800">
         <Navbar onToggleTheme={toggleDarkMode} />
         <InfoSection />
-        <CardSection />
+        <CardSection
+          title="Values"
+          imagesSrcList={[
+            {
+              src: "/images/design.png",
+              type: "image/png",
+              description: "Design",
+              subtitle: "Bring beautiful design prototypes to life",
+            },
+            {
+              src: "/images/code.png",
+              type: "image/png",
+              description: "Code",
+              subtitle: "Emphasis on writing clean code & reusable components",
+            },
+            {
+              src: "/images/consulting.png",
+              type: "image/png",
+              description: "Collaborate",
+              subtitle:
+                "Collaborate with the team members to learn from each other",
+            },
+          ]}
+        />
+        <ProjectSection
+          title="Projects"
+          imagesSrcList={[
+            {
+              src: "/images/recipe.png",
+              type: "image/png",
+              description: "Recipe App",
+            },
+            {
+              src: "/images/nft-challenge.png",
+              type: "image/png",
+              description: "Friendly NFT marketplace",
+            },
+            {
+              src: "/images/whatsapp.png",
+              type: "image/png",
+              description: "WhatsApp Web clone",
+            },
+          ]}
+        />
       </div>
     </div>
   );
--- src/components/ProjectSection/index.tsx
+++ src/components/ProjectSection/index.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import FallbackImage from "../FallbackMedia/images";
-import { ICard, TImageSrc } from "../ProjectSection";
 
-const CardSection = (props: ICard) => {
+export type TImageSrc = {
+  src: string;
+  type: string;
+  description: string;
+  subtitle?: string;
+};
+
+export interface ICard {
+  title: string;
+  imagesSrcList: TImageSrc[];
+}
+
+const ProjectSection = (props: ICard) => {
   const { title, imagesSrcList } = props;
   return (
-    <div className="flex flex-col text-center justify-center items-center my-10">
+    <div className="flex flex-col text-center justify-center items-center mt-10">
       <h3 className="text-3xl py-1 dark:text-white">{title}</h3>
+
       <div className="flex flex-col lg:flex-row justify-center items-center gap-8 py-10 dark:text-white">
         {imagesSrcList.map((imageSrc: TImageSrc, index) => (
-          <div className="text-center p-10 shadow-lg rounded-xl w-80 h-80 dark:bg-slate-900">
+          <div className="text-center shadow-lg rounded-xl w-80 h-96 dark:bg-slate-900 flex flex-col justify-between">
             <FallbackImage
               original={{
                 src: imageSrc.src,
@@ -19,15 +31,10 @@ const CardSection = (props: ICard) => {
                 src: imageSrc.src,
                 type: imageSrc.type,
               }}
-              className="w-20 h-20 mx-auto my-4"
+              className="rounded-xl mb-4 h-60 w-full"
               loading="lazy"
             />
-            <h1 className="text-lg font-medium">{imageSrc.description}</h1>
-            {imageSrc.subtitle && (
-              <p className="text-gray-800 dark:text-white mt-4">
-                {imageSrc.subtitle}
-              </p>
-            )}
+            <h1 className="text-lg font-medium p-10">{imageSrc.description}</h1>
           </div>
         ))}
       </div>
@@ -35,4 +42,4 @@ const CardSection = (props: ICard) => {
   );
 };
 
-export default CardSection;
+export default ProjectSection;
